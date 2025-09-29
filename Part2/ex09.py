b0567486e9e02ed49e44453f2d0c4d90e0154246
amjadrifaie <amjadrezal127@gmail.com>
2025-09-29 06:06:20 +0000

    I have finished Part 1

--- Part2/ex09.py
+++ Part2/ex09.py
@@ -1,1 +1,9 @@
 
+# Create a function called my_function
+# that prints "Hi Marvin!" when called
+
+def my_function():
+    print("Hi Marvin!")
+
+# Call the function
+my_function()
---
+++ Part2/ex10.py
@@ -0,0 +1,6 @@
+# Create a function called say_hi that takes one argument (name)
+# It should display a string like: "Greetings Marvin!" or "Greetings Ben!"
+def say_hi():
+  print("Greetings Marvin!")
+
+say_hi()
